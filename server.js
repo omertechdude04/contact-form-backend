22c19378741fbf68fa5382631ec1d74c18b63333
omertechdude04 <omertaib04@gmail.com>
2025-05-09 17:02:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.post('/send', async (req, res) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }       
+    },
+    secure: false
   });
 
   const mailOptions = {
